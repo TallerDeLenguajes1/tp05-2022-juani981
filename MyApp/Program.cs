e1171e0b816a635fbffbb017851d72ce60c088a7
juani981 <juanicarrizo97@gmail.com>
2022-05-17 16:34:19 -0300

    commit con ramas

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -2,3 +2,11 @@
 Console.WriteLine("Hola,mundo!");
 Console.WriteLine("La hora es " + DateTime.Now);
 Console.WriteLine("Nuevo commit");
+Console.WriteLine("Escriba su nombre");
+string x=Console.ReadLine();
+Console.WriteLine("Su nombre es:");
+Console.WriteLine(x);
+
+Console.WriteLine("Este texto solo es visible en rama 2");
+
+Console.WriteLine("Este texto solo es visible en rama 3");
