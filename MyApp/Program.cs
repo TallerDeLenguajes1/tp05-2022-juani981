e191956603fc388ef8121a2d18c627c6866bcccb
juani981 <juanicarrizo97@gmail.com>
2022-06-24 00:11:44 -0300

    Creacion Branch Calculadora V1

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,11 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hola,mundo!");
-Console.WriteLine("La hora es " + DateTime.Now);
-Console.WriteLine("Nuevo commit");
-Console.WriteLine("Escriba su nombre");
-string x=Console.ReadLine();
-Console.WriteLine("Su nombre es:");
-Console.WriteLine(x);
+﻿
 
-Console.WriteLine("Este texto solo es visible en main");
 
